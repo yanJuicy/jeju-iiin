9d948e3d9a221ac7ec0abf501a2eb96088769926
minho <minhokim0806@gmail.com>
2022-12-23 20:07:37 +0900

    feat: initial commit

---
+++ src/test/java/com/example/jejuiiin/JejuIiinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jejuiiin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuIiinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
