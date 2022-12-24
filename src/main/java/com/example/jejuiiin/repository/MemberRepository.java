54b19f67485f770f1ca8f6b4b39ec997456db886
minho <minhokim0806@gmail.com>
2022-12-24 13:30:43 +0900

    feat: 인증 객체를 반환하는 `UserDetailsServiceImpl` 클래스에 필요한 내용 구현
    1. 인증 객체인 `UserDetailsImpl`을 반환하는 `loadUserByUsername` 메서드 구현
    
    2. 위의 메서드를 구현하기 위해 `MemberRepository`에 `findByLoginId` 메서드 구현

--- src/main/java/com/example/jejuiiin/repository/MemberRepository.java
+++ src/main/java/com/example/jejuiiin/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.example.jejuiiin.repository;
 import com.example.jejuiiin.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>{
+    Optional<Member> findByLoginId(String loginId);
 }
--- src/main/java/com/example/jejuiiin/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/jejuiiin/security/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.example.jejuiiin.security;
 
-public class UserDetailsServiceImpl {
+import com.example.jejuiiin.domain.Member;
+import com.example.jejuiiin.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+//인증 서비스
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        // 사용자 조회
+        Member member = memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+        // UserDetailsImpl 반환
+        return new UserDetailsImpl(member, member.getLoginId(), member.getPassword());
+    }
 }
