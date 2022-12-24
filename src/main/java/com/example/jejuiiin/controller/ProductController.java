778ed72f5865e2d1993051f6276e00d7d54f380f
sooni2 <jinong22@gmail.com>
2022-12-24 21:01:15 +0900

    feat - 상세 페이지 조회 기능 추가
    Controller, Service, Mapper, ProductDetailResponse 추가

--- src/main/java/com/example/jejuiiin/controller/ProductController.java
+++ src/main/java/com/example/jejuiiin/controller/ProductController.java
@@ -1,6 +1,11 @@
 package com.example.jejuiiin.controller;
 
+
+import com.example.jejuiiin.mapper.ProductDetailResponse;
+import com.example.jejuiiin.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{productId}")
+    public ProductDetailResponse getProduct(@PathVariable Long productId) {
+        return productService.getProduct(productId);
+    }
+
+
 }
