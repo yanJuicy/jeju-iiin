7e8562a6cc7499c266af9b26375d3fb8c0b68f6b
황보석 <zheoajfl@gmail.com>
2022-12-24 12:02:40 +0900

    feat: 신규 상품 보여주기 (노출)기능

--- src/main/java/com/example/jejuiiin/service/ProductService.java
+++ src/main/java/com/example/jejuiiin/service/ProductService.java
@@ -1,7 +1,27 @@
 package com.example.jejuiiin.service;
 
+import com.example.jejuiiin.controller.response.ProductResponse;
+import com.example.jejuiiin.domain.Product;
+import com.example.jejuiiin.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public List<ProductResponse> getProducts() {
+        List<ProductResponse> list = new ArrayList<>();
+        List<Product> products = productRepository.findTop5ByOrderByProductIdDesc();
+        for (Product product : products) {
+            list.add(new ProductResponse(product));
+        }
+        return list;
+    }
 }
