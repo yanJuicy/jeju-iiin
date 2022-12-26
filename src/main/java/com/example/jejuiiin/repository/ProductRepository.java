8d4440a5941a3fe6b61e0b11f6a92e3f42531136
yanJuicy <hanjub3@gmail.com>
2022-12-26 13:51:56 +0900

    feat: category, subcategory 값을 기준으로 Product 조회하는 메소드 작성
    category와 일치하는 product 조회
    subCategory와 일치하는 product 조회
    Pageable의 인자 만큼
    page, size 갯수 만큼 조회

--- src/main/java/com/example/jejuiiin/repository/ProductRepository.java
+++ src/main/java/com/example/jejuiiin/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.example.jejuiiin.repository;
 
 import com.example.jejuiiin.domain.Product;
 import com.example.jejuiiin.domain.ProductCategory;
+import com.example.jejuiiin.domain.ProductSubCategory;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +12,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findTop5ByOrderByProductIdDesc(); //신규 상품 기준 5개 노출
+	List<Product> findTop5ByOrderByProductIdDesc(); //신규 상품 기준 5개 노출
 
-    Page<Product> findAllByCategoryOrderByProductIdDesc(@Param("category") ProductCategory category, Pageable pageable);
+	Page<Product> findAllByCategoryOrSubCategoryOrderByProductIdDesc(@Param("category") ProductCategory category,
+		@Param("subCategory") ProductSubCategory subCategory, Pageable pageable);
 }
