d96496b4d13788f9b2f9009eebc4fa5e21c5a351
minho <minhokim0806@gmail.com>
2022-12-26 17:30:39 +0900

    !HOTFIX: Security에 CORS 적용

--- src/main/java/com/example/jejuiiin/security/WebSecurityConfig.java
+++ src/main/java/com/example/jejuiiin/security/WebSecurityConfig.java
@@ -13,6 +13,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -27,8 +32,9 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        /* 1. CSRF(Cross-site request forgery) */
-        http.csrf().disable();
+        /* 1. CSRF(Cross-site request forgery) 비활성화 및 CORS 설정 */
+        http.csrf().disable()
+                .cors().configurationSource(corsConfigurationSource());
 
         /* 2. Session 비활성화 */
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
@@ -44,5 +50,21 @@ public class WebSecurityConfig {
         http.addFilterBefore(new JwtExceptionHandlerFilter(), JwtAuthFilter.class);
 
         return http.build();
+    }
+
+    // cors 설정
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("http://4team-troubleshooter.s3-website.ap-northeast-2.amazonaws.com/");
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT")); // 허용할 Http Method
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true); // 내 서버가 응답할 때 json을 js에서 처리할 수 있게 설정
+        configuration.setMaxAge(3600L);
+        configuration.addExposedHeader("authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
     }
 }
