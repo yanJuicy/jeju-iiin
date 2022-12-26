b028341d52771cbdb1532ee75eea5dd1b3fd661b
minho <minhokim0806@gmail.com>
2022-12-26 11:56:03 +0900

    feat: MemberController에 회원가입 요청 추가
    /api/v1/members/signup으로 들어오는 회원가입 요청을 처리하기 위한 signup 메소드 구현

--- src/main/java/com/example/jejuiiin/controller/MemberController.java
+++ src/main/java/com/example/jejuiiin/controller/MemberController.java
@@ -1,6 +1,12 @@
 package com.example.jejuiiin.controller;
 
+import com.example.jejuiiin.controller.request.SignupRequest;
+import com.example.jejuiiin.controller.response.Response;
+import com.example.jejuiiin.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public Response signup(@Validated @RequestBody SignupRequest request){
+        Signup signup = Signup.builder()
+                .loginId(request.getLoginId())
+                .password(request.getPassword())
+                .name(request.getName())
+                .email(request.getEmail())
+                .isSocial(false)
+                .socialType(null)
+                .build();
+
+        MemberService.signup(signup);
+
+        return new Response(201, "회원가입이 완료되었습니다.", null);
+    }
 }
