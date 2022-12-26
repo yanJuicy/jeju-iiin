ae4a131e342fa7c081e86c525535355360d21105
minho <minhokim0806@gmail.com>
2022-12-26 17:03:43 +0900

    feat: MemberMapper 클래스 구현
    SignupServiceRequest를 Member 엔티티로 매핑해주는`signupServiceRequestToMember` 메소드 구현

--- src/main/java/com/example/jejuiiin/mapper/MemberMapper.java
+++ src/main/java/com/example/jejuiiin/mapper/MemberMapper.java
@@ -1,4 +1,19 @@
 package com.example.jejuiiin.mapper;
 
+import com.example.jejuiiin.controller.request.SignupServiceRequest;
+import com.example.jejuiiin.domain.Member;
+
 public class MemberMapper {
+
+    /* Signup Dto를 Member 엔티티로 매핑 */
+    public static Member signupServiceRequestToMember(SignupServiceRequest signupServiceRequest, String password){
+        return Member.builder()
+                .loginId(signupServiceRequest.getLoginId())
+                .password(password)
+                .name(signupServiceRequest.getName())
+                .email(signupServiceRequest.getEmail())
+                .isSocial(signupServiceRequest.isSocial())
+                .socialType(signupServiceRequest.getSocialType())
+                .build();
+    }
 }
