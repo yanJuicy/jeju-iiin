adad2200af9f114bc3f8760a165719b0d5b11c7f
yanJuicy <hanjub3@gmail.com>
2022-12-26 23:17:09 +0900

    feat: 장바구니 상품 등록 API 작성

--- src/main/java/com/example/jejuiiin/service/CartService.java
+++ src/main/java/com/example/jejuiiin/service/CartService.java
@@ -1,7 +1,49 @@
 package com.example.jejuiiin.service;
 
+import com.example.jejuiiin.controller.request.CreateCartItemRequest;
+import com.example.jejuiiin.controller.response.CreateCartItemResponse;
+import com.example.jejuiiin.domain.CartItem;
+import com.example.jejuiiin.domain.Member;
+import com.example.jejuiiin.domain.Product;
+import com.example.jejuiiin.repository.CartRepository;
+import com.example.jejuiiin.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+
+import static com.example.jejuiiin.controller.exception.ExceptionMessage.NO_EXISTS_PRODUCT_MSG;
 
 @Service
+@RequiredArgsConstructor
 public class CartService {
+
+    private final CartRepository cartRepository;
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public CreateCartItemResponse createCartItem(CreateCartItemRequest request, Member loginMember) {
+        Long productId = request.getProductId();
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new NoSuchElementException(NO_EXISTS_PRODUCT_MSG.getMsg()));
+
+        // mapper 변환 필요
+        CartItem cartItem = CartItem.builder()
+                .member(loginMember)
+                .productId(productId)
+                .thumbnail_img_url(product.getThumbnailImgUrl())
+                .name(product.getName())
+                .sellingPrice(product.getPrice())
+                .quantity(request.getQuantity())
+                .summation(request.getQuantity() * product.getPrice())
+                .build();
+
+        CartItem newCartItem = cartRepository.save(cartItem);
+        return new CreateCartItemResponse(newCartItem.getCartItemId());
+    }
+
+
 }
