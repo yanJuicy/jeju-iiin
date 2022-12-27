f8b4e3a92496c30eb84001edcb8712a39405af82
황보석 <zheoajfl@gmail.com>
2022-12-27 23:39:22 +0900

    feat: 장바구니 조회 API

---
+++ src/main/java/com/example/jejuiiin/controller/response/MyCartResponse.java
@@ -0,0 +1,27 @@
+package com.example.jejuiiin.controller.response;
+
+
+import lombok.Getter;
+import lombok.Builder;
+@Getter
+@Builder
+public class MyCartResponse {
+    private Long productId;
+    private String thumbnailImgUrl;
+    private String name;
+    private int sellingPrice;
+    private int quantity;
+    private int summation;
+
+    public MyCartResponse(Long productId, String thumbnailImgUrl, String name, int sellingPrice, int quantity, int summation) {
+        this.productId = productId;
+        this.thumbnailImgUrl = thumbnailImgUrl;
+        this.name = name;
+        this.sellingPrice = sellingPrice;
+        this.quantity = quantity;
+        this.summation = summation;
+    }
+
+
+
+}
