7e7410b3f0727f2af912b60a453cebb4ca03e18d
minho <minhokim0806@gmail.com>
2022-12-28 21:40:22 +0900

    feat: `ServiceUtil`에 `findMember` 메소드 추가

--- src/main/java/com/example/jejuiiin/service/util/ServiceUtil.java
+++ src/main/java/com/example/jejuiiin/service/util/ServiceUtil.java
@@ -4,20 +4,17 @@ import com.example.jejuiiin.domain.CartItem;
 import com.example.jejuiiin.domain.Member;
 import com.example.jejuiiin.domain.Product;
 import com.example.jejuiiin.repository.CartRepository;
+import com.example.jejuiiin.repository.MemberRepository;
 import com.example.jejuiiin.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
-import static com.example.jejuiiin.controller.exception.ExceptionMessage.NO_EXISTS_CART_ITEM_MSG;
-import static com.example.jejuiiin.controller.exception.ExceptionMessage.NO_EXISTS_PRODUCT_MSG;
+import static com.example.jejuiiin.controller.exception.ExceptionMessage.*;
 
-@Service
-@RequiredArgsConstructor
 public class ServiceUtil {
-    private static final ProductRepository productRepository;
-    private static final CartRepository cartRepository;
+    private static ProductRepository productRepository;
+    private static CartRepository cartRepository;
+    private static MemberRepository memberRepository;
 
     public static Product findProduct(Long productId) {
         return productRepository.findById(productId)
@@ -27,5 +24,15 @@ public class ServiceUtil {
     public static CartItem findCartItem(Long productId, Member member) {
         return cartRepository.findByProductIdAndMember(productId, member)
                 .orElseThrow(() -> new NoSuchElementException(NO_EXISTS_CART_ITEM_MSG.getMsg()));
+    }
+
+    public static Member findMember(String loginId) {
+        return memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new NoSuchElementException(NO_EXIST_MEMBER_MSG.getMsg()));
+    }
+
+    public static Member findMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NoSuchElementException(NO_EXIST_MEMBER_MSG.getMsg()));
     }
 }
