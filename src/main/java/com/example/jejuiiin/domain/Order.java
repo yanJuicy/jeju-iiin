6efbfa8f1fc5e6f0eeca1d69d669c8f0b0186ff0
minho <minhokim0806@gmail.com>
2022-12-28 21:57:46 +0900

    fix: Order 엔티티 테이블 이름 변경
    Order 엔티티 테이블 이름 Orders로 변경. Order가 이미 있는 이름이라 그런지 오류가 났음.

--- src/main/java/com/example/jejuiiin/domain/Order.java
+++ src/main/java/com/example/jejuiiin/domain/Order.java
@@ -4,25 +4,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Getter
+@Table(name = "ORDERS")
 @NoArgsConstructor
 @Entity
-public class Order {
+public class Order extends TimeStamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "MEMBER_ID", nullable = false)
     private Member member;
 
