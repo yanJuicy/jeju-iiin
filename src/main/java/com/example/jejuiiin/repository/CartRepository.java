589db5557baf6e77dc7d89374f576a266e4893bc
minho <minhokim0806@gmail.com>
2022-12-29 16:40:42 +0900

    refactor: 주문 시 장바구니 삭제 쿼리 변경

--- src/main/java/com/example/jejuiiin/repository/CartRepository.java
+++ src/main/java/com/example/jejuiiin/repository/CartRepository.java
@@ -4,6 +4,9 @@ import com.example.jejuiiin.domain.CartItem;
 import com.example.jejuiiin.domain.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +18,8 @@ public interface CartRepository extends JpaRepository<CartItem, Long> {
     int countByMember(Member member);
 
     List<CartItem> findAllByMember(Member member);
+
+    @Modifying
+    @Query("delete from CartItem c where c.productId in :ids")
+    void deleteAllByProductIdInQuery(@Param("ids") List<Long> ids);
 }
